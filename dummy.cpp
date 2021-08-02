e26a9d791b3dc1335e306b2b3dda63b1395fdafa
hanzlaxoho <hanzlak@xoho.tech>
2021-08-02 17:29:37 +0500

    another another file k sath py file bhi add ki hy

--- dummy.cpp
+++ dummy.cpp
@@ -5,6 +5,7 @@ int main(){
 
     cout << "Hello friends!...github seekh lo! \n";
     cout << "another line | some changes to it";
+    cout << "another another line";
 
     return 0;
 }
---
+++ py_test.py
@@ -0,0 +1,1 @@
+print('yeh hy mango zindagi!')
